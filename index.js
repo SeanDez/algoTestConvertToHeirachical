9fce1092dadf46d8e7c15c11362525c5ef9f4b83
Sean Dez <sd@seandezoysa.com>
2020-11-29 11:11:00 -0500

    solve problem with tests

--- index.js
+++ index.js
@@ -30,17 +30,124 @@ constraints
 */
 
 
-
-/** tree sorts a json string and returns again as a string
- * @param: jsonString: string.
- * @return: string. JSON string, sorted
+/*
+  null parent ids go first
+  then rest
 */
-const sortByParentsFirst(jsonString) {
-  const object = JSON.parse(jsonString);
 
 
-  const reStringed = JSON.stringify(null);
-  return reStringed;
+class ProductCategoryNode {
+  name; 
+  id; 
+  parentId;
+
+  constructor(name, id, parentId = null) {
+    this.name = name;
+    this.id = id;
+    this.parent_id = parentId;
+  }
 }
 
+const mockData =  [
+  {
+    "name": "Accessories",
+    "id": 1,
+    "parent_id": 20,
+  }, {
+    "name": "Watches",
+    "id": 57,
+    "parent_id": 1
+  }, {
+    "name": "Board Games",
+    "id": 45,
+    "parent_id": 20,
+  }, {
+    "name": "Model Planes",
+    "id": 1,
+    "parent_id": null,
+  }, {
+    "name": "Doll Houses",
+    "id": 31,
+    "parent_id": 20,
+  }, {
+    "name": "Men",
+    "id": 20,
+    "parent_id": null,
+  }, {
+    "name": "Music",
+    "id": 205,
+    "parent_id": 45,
+  }, {
+    "name": "Women",
+    "id": 220,
+    "parent_id": 45,
+  }, {
+    "name": "Baby",
+    "id": 2110,
+    "parent_id": 45,
+  }, 
+  
+  // intentional duplicates
+  {
+    "name": "Baby",
+    "id": 2110,
+    "parent_id": null,
+  }, {
+    "name": "Baby",
+    "id": 2110,
+    "parent_id": 1,
+  }, {
+    "name": "Baby",
+    "id": 2110,
+    "parent_id": 220,
+  }
+];
 
+/** sorts so that parents come before children
+ * @param: categories. object array
+ * @return: object array
+*/
+function sortByParentsFirst(categories) {
+  const parsedCategories = JSON.parse(categories);
+
+  // type guard against non-array data type
+  if (Array.isArray(parsedCategories) === false) { return []; }
+
+  const sortedResults = [];
+
+  let matchableParentIds = [null];
+  let nexParentIdsToCheckAgainst = [];
+  let fullySorted = false;
+
+  while (fullySorted === false) {
+    for (let idx = 0; idx < parsedCategories.length; idx++) {
+      const currentCategory = parsedCategories[idx];
+  
+      const matchesAParentId = matchableParentIds.some(matchableParentId => currentCategory.parent_id === matchableParentId);
+  
+      // adds current category to results
+      // adds its id as a parent on the next full loop cycle
+      if (matchesAParentId) { 
+        sortedResults.push(currentCategory);
+        
+        // delete item from unsorted
+        parsedCategories.splice(idx, 1);
+
+        matchableParentIds.push(currentCategory.id);
+      }
+    }
+    // full loop cycle completes
+
+    // loop exit condition
+    if (parsedCategories.length === 0) { fullySorted = true; }
+  }
+
+  const jsonString = JSON.stringify(sortedResults)
+
+  return jsonString;
+}
+
+// const results = sortByParentsFirst(mockData);
+// console.log('results', results);
+
+module.exports = sortByParentsFirst;
